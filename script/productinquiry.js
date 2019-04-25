33379ce9c56299aa86b17a13200b85a82d69c8b2
yangfan <yangfan1987816@gmail.com>
2019-04-25 10:17:25 +0800

    edit

---
+++ script/productinquiry.js
@@ -0,0 +1,20 @@
+var vm = new Vue({
+    el: '#inquiry',
+    data: {
+        quantity1: "g",
+        quantity2: "g",
+        purity: "",
+        shippingTo: "",
+        addtionalRequest: "",
+        firstName: "",
+        lastName: "",
+        email: "",
+        company: "",
+        phone: ""
+    },
+    methods: {
+        submit: function () {
+            
+        }
+    }
+});
---
+++ script/quickinquiry.js
@@ -0,0 +1,22 @@
+var vm = new Vue({
+    el: '#inquiry',
+    data: {
+        CASNO: "",
+        productName: "",
+        quantity1: "g",
+        quantity2: "g",
+        purity: "",
+        shippingTo: "",
+        addtionalRequest: "",
+        firstName: "",
+        lastName: "",
+        email: "",
+        company: "",
+        phone: ""
+    },
+    methods: {
+        submit: function () {
+
+        }
+    }
+});
